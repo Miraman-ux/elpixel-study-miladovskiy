8858d3b66ca8b6b1f4a6900cefe3dac8b22d082b
M1raman <vladgegat@gmail.com>
2026-06-08 22:02:49 +0300

    solved tasks for levels 1.1

---
+++ level-1.1.js
@@ -0,0 +1,53 @@
+/*
+№1 Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
+ */
+
+let checkedNumber = 67
+
+if (checkedNumber < 0) {
+    console.log("Negative")
+}else{
+    console.log("Positive")
+}
+
+/*
+Дана строка. Выведите в консоль длину этой строки.
+ */
+
+let testStringLen = 'testString12345'
+console.log(testStringLen.length)
+
+/*
+Дана строка. Выведите в консоль последний символ строки.
+ */
+
+let testStringLast = 'testString1234f'
+let lastChar = testStringLast.slice(-1)
+console.log(lastChar)
+
+/*
+Дано число. Проверьте, четное оно или нет.
+ */
+
+let evenOrNotNumber = 56
+if (evenOrNotNumber % 2 === 0) {
+    console.log("Even")
+} else {
+    console.log("NotEven")
+}
+
+/*
+Даны два слова. Проверьте, что первые буквы этих слов совпадают.
+ */
+
+let testString1 = 'testString'
+let testString2 = 'testString2'
+
+let firstChar1 = testString1[0].toLowerCase()
+let secondChar1 = testString1[0].toLowerCase()
+
+if (firstChar1 === secondChar1) {
+    console.log("Match")
+}else {
+    console.log("NotMatch")
+}
