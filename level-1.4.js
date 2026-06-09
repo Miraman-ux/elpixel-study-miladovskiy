e08c7fdd88191a2d34e073caa29a6caccd116d71
M1raman <276498452+Miraman-ux@users.noreply.github.com>
2026-06-09 11:19:16 +0300

    solved tasks for levels 1.4

---
+++ level-1.4.js
@@ -0,0 +1,47 @@
+/*
+№1 Дана строка: 'abcde' Получите массив букв этой строки.
+ */
+
+task1String = 'abcde'
+task1Array = task1String.split('')
+console.log(task1Array)
+
+/*
+№2 Дано некоторое число: 12345 Получите массив цифр этого числа.
+ */
+
+task2Number = 12345
+task2String = task2Number.toString().split('').map(Number)
+console.log(task2String)
+
+/*
+№3
+Дано некоторое число: 12345
+Переверните его: 54321
+ */
+
+task3Number = 12345
+task3String = task3Number.toString()
+task3Array = []
+
+for (let i = 0; i < task3String.length; i++) {
+    task3Array.unshift(+task3String[i])
+}
+
+console.log(task3Array)
+
+/*
+Дано некоторое число: 12345 Найдите сумму цифр этого числа.
+ */
+
+let task4Number = 12345
+let task4String = task4Number.toString()
+let task4Array = []
+let sum = 0
+
+for (let i = 0; i < task4String.length; i++) {
+    task4Array.push(+task4String[i])
+    sum += task4Array[i]
+}
+
+console.log(sum)
